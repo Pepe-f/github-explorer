ed29367c873c0114a2f6463aa827e57f783d6232
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2023-03-26 02:45:39 +0300

    Styling Dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,56 @@
 import { FC } from 'react'
+import { FiChevronRight } from 'react-icons/fi'
 
-import { Title } from './styles'
+import logoImg from '../../assets/logo.svg'
+
+import { Form, Repositories, Title } from './styles'
 
 const Dashboard: FC = () => {
-  return <Title>Explore repositories in GitHub</Title>
+  return (
+    <>
+      <img src={logoImg} alt='GitHub Explorer' />
+      <Title>Explore repositories in GitHub</Title>
+      <Form>
+        <input placeholder='Enter the name of the repository' />
+        <button type='submit'>Search</button>
+      </Form>
+      <Repositories>
+        <a href='test'>
+          <img
+            src='https://avatars3.githubusercontent.com/u/50633599?v=4'
+            alt='Elias Gabriel'
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Fodasi</p>
+          </div>
+          <FiChevronRight size={20} color='#cbcbd6' />
+        </a>
+        <a href='test'>
+          <img
+            src='https://avatars3.githubusercontent.com/u/50633599?v=4'
+            alt='Elias Gabriel'
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Fodasi</p>
+          </div>
+          <FiChevronRight size={20} color='#cbcbd6' />
+        </a>
+        <a href='test'>
+          <img
+            src='https://avatars3.githubusercontent.com/u/50633599?v=4'
+            alt='Elias Gabriel'
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Fodasi</p>
+          </div>
+          <FiChevronRight size={20} color='#cbcbd6' />
+        </a>
+      </Repositories>
+    </>
+  )
 }
 
 export default Dashboard
